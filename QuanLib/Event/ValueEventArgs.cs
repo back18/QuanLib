f24b060a1efcf28b0fd9ca5da16534c0d941cb11
Quan <2200191123@qq.com>
2023-08-03 02:23:13 +0800

    ValueEventArgs

---
+++ QuanLib/Event/ValueEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Event
+{
+    public class ValueEventArgs<T> : EventArgs
+    {
+        public ValueEventArgs(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; }
+    }
+}
