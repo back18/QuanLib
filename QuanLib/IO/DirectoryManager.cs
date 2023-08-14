06551b84ea1d18640e5a1e045e58b124fb8ca104
Quan <2200191123@qq.com>
2023-08-14 14:22:28 +0800

    DirectoryManager

---
+++ QuanLib/IO/DirectoryManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.IO
+{
+    public class DirectoryManager
+    {
+        public DirectoryManager(string directory)
+        {
+            Directory = directory ?? throw new ArgumentNullException(nameof(directory));
+        }
+
+        public string Directory { get; }
+
+        public string Combine(string path)
+        {
+            return Path.Combine(Directory, path);
+        }
+
+        public override string ToString()
+        {
+            return Directory;
+        }
+    }
+}
