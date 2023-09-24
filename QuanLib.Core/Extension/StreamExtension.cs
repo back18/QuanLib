587afb0887e37d4046cc62d64a8a564beaea354e
Quan <2200191123@qq.com>
2023-09-24 19:06:02 +0800

    完善 StreamExtension

--- QuanLib.Core/Extension/StreamExtension.cs
+++ QuanLib.Core/Extension/StreamExtension.cs
@@ -10,12 +10,30 @@ namespace QuanLib.Core.Extension
     {
         public static string ToUtf8Text(this Stream stream)
         {
-            using StreamReader reader = new(stream, Encoding.UTF8);
-            return reader.ReadToEnd();
+            return ToText(stream, Encoding.UTF8);
         }
 
         public static string[] ToUtf8TextLines(this Stream stream)
         {
+            return ToTextLines(stream, Encoding.UTF8);
+        }
+
+        public static string ToText(this Stream stream, Encoding encoding)
+        {
+            if (encoding is null)
+                throw new ArgumentNullException(nameof(encoding));
+
+            stream.Seek(0, SeekOrigin.Begin);
+            using StreamReader reader = new(stream, encoding);
+            return reader.ReadToEnd();
+        }
+
+        public static string[] ToTextLines(this Stream stream, Encoding encoding)
+        {
+            if (encoding is null)
+                throw new ArgumentNullException(nameof(encoding));
+
+            stream.Seek(0, SeekOrigin.Begin);
             using StreamReader reader = new(stream, Encoding.UTF8);
             List<string> lines = new();
             while (!reader.EndOfStream)
