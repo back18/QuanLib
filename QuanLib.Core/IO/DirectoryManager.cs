56635cfc6d4983c843f00524214b3525c4933a83
Quan <2200191123@qq.com>
2023-09-27 03:46:04 +0800

    完善DirectoryManager

--- QuanLib.Core/IO/DirectoryManager.cs
+++ QuanLib.Core/IO/DirectoryManager.cs
@@ -1,19 +1,63 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace QuanLib.Core.IO
 {
-    public class DirectoryManager
+    public class DirectoryManager : IReadOnlyDictionary<string, DirectoryManager>
     {
         public DirectoryManager(string directory)
         {
-            FullPath = directory ?? throw new ArgumentNullException(nameof(directory));
+            if (string.IsNullOrEmpty(directory))
+                throw new ArgumentException($"“{nameof(directory)}”不能为 null 或空。", nameof(directory));
+
+            FullPath = Path.GetFullPath(directory);
+            DictionaryName = Path.GetFileName(directory);
+
+            _items = new();
         }
 
+        private readonly Dictionary<string, DirectoryManager> _items;
+
         public string FullPath { get; }
+
+        public string DictionaryName { get; }
+
+        public IEnumerable<string> Keys => _items.Keys;
+
+        public IEnumerable<DirectoryManager> Values => _items.Values;
+
+        public int Count => _items.Count;
+
+        public DirectoryManager this[string key] => _items[key];
+
+        public T AddDirectory<T>(string name) where T : DirectoryManager
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentException($"“{nameof(name)}”不能为 null 或空。", nameof(name));
+
+            Type type = typeof(T);
+            ConstructorInfo[] constructors = type.GetConstructors(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                ParameterInfo[] parameters = constructor.GetParameters();
+                if (parameters.Length == 1 && parameters[0].ParameterType == typeof(string))
+                {
+                    object instance = constructor.Invoke(new object[] { Combine(name) });
+                    T directory = (T)instance;
+                    _items.Add(directory.DictionaryName, directory);
+                    return directory;
+                }
+            }
+
+            throw new ArgumentException("找不到合适的构造函数");
+        }
 
         public string Combine(string path)
         {
@@ -80,9 +124,36 @@ namespace QuanLib.Core.IO
             return Directory.GetDirectories(FullPath, searchPattern);
         }
 
+        public void BuildDirectoryTree()
+        {
+            CreateIfNotExists();
+            foreach (DirectoryManager directory in _items.Values)
+                directory.BuildDirectoryTree();
+        }
+
         public override string ToString()
         {
             return FullPath;
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return _items.ContainsKey(key);
+        }
+
+        public bool TryGetValue(string key, [MaybeNullWhen(false)] out DirectoryManager value)
+        {
+            return _items.TryGetValue(key, out value);
+        }
+
+        public IEnumerator<KeyValuePair<string, DirectoryManager>> GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)_items).GetEnumerator();
         }
     }
 }
