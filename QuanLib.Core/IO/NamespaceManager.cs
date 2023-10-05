a741cbd1e2d7f39f25ae40d0b61a5f9d0b44200f
Quan <2200191123@qq.com>
2023-10-06 02:36:51 +0800

    新增命名空间管理工具

---
+++ QuanLib.Core/IO/NamespaceManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core.IO
+{
+    public class NamespaceManager : IReadOnlyDictionary<string, NamespaceManager>
+    {
+        public NamespaceManager(string @namespace)
+        {
+            if (string.IsNullOrEmpty(@namespace))
+                throw new ArgumentException($"“{nameof(@namespace)}”不能为 null 或空。", nameof(@namespace));
+
+            FullName = @namespace;
+
+            _items = new();
+        }
+
+        public string FullName { get; }
+
+        private readonly Dictionary<string, NamespaceManager> _items;
+
+        public NamespaceManager this[string key] => _items[key];
+
+        public IEnumerable<string> Keys => _items.Keys;
+
+        public IEnumerable<NamespaceManager> Values => _items.Values;
+
+        public int Count => _items.Count;
+
+        public T AddNamespace<T>(string name) where T : NamespaceManager
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentException($"“{nameof(name)}”不能为 null 或空。", nameof(name));
+
+            Type type = typeof(T);
+            ConstructorInfo[] constructors = type.GetConstructors(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                ParameterInfo[] parameters = constructor.GetParameters();
+                if (parameters.Length == 1 && parameters[0].ParameterType == typeof(string))
+                {
+                    object instance = constructor.Invoke(new object[] { Combine(name) });
+                    T @namespace = (T)instance;
+                    _items.Add(@namespace.FullName, @namespace);
+                    return @namespace;
+                }
+            }
+
+            throw new ArgumentException("找不到合适的构造函数");
+        }
+
+        public string Combine(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                throw new ArgumentException($"“{nameof(path)}”不能为 null 或空。", nameof(path));
+
+            return Namespace.Combine(FullName, path);
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return _items.ContainsKey(key);
+        }
+
+        public bool TryGetValue(string key, [MaybeNullWhen(false)] out NamespaceManager value)
+        {
+            return _items.TryGetValue(key, out value);
+        }
+
+        public IEnumerator<KeyValuePair<string, NamespaceManager>> GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)_items).GetEnumerator();
+        }
+    }
+}
---
+++ QuanLib.Core/Namespace.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    public static class Namespace
+    {
+        private const char CHAR = '.';
+
+        public static string Combine(string path1, string path2)
+        {
+            if (string.IsNullOrEmpty(path1))
+                throw new ArgumentException($"“{nameof(path1)}”不能为 null 或空。", nameof(path1));
+            if (string.IsNullOrEmpty(path2))
+                throw new ArgumentException($"“{nameof(path2)}”不能为 null 或空。", nameof(path2));
+
+            path1 = path1.TrimEnd(CHAR);
+            path2 = path2.TrimStart(CHAR);
+            return $"{path1}.{path2}";
+        }
+
+        public static string Combine(string path1, string path2, string path3)
+        {
+            if (string.IsNullOrEmpty(path1))
+                throw new ArgumentException($"“{nameof(path1)}”不能为 null 或空。", nameof(path1));
+            if (string.IsNullOrEmpty(path2))
+                throw new ArgumentException($"“{nameof(path2)}”不能为 null 或空。", nameof(path2));
+            if (string.IsNullOrEmpty(path3))
+                throw new ArgumentException($"“{nameof(path3)}”不能为 null 或空。", nameof(path3));
+
+            path1 = path1.TrimEnd(CHAR);
+            path2 = path2.Trim(CHAR);
+            path3 = path3.TrimStart(CHAR);
+            return $"{path1}.{path2}.{path3}";
+        }
+
+        public static string Combine(string path1, string path2, string path3, string path4)
+        {
+            if (string.IsNullOrEmpty(path1))
+                throw new ArgumentException($"“{nameof(path1)}”不能为 null 或空。", nameof(path1));
+            if (string.IsNullOrEmpty(path2))
+                throw new ArgumentException($"“{nameof(path2)}”不能为 null 或空。", nameof(path2));
+            if (string.IsNullOrEmpty(path3))
+                throw new ArgumentException($"“{nameof(path3)}”不能为 null 或空。", nameof(path3));
+            if (string.IsNullOrEmpty(path4))
+                throw new ArgumentException($"“{nameof(path4)}”不能为 null 或空。", nameof(path4));
+
+            path1 = path1.TrimEnd(CHAR);
+            path2 = path2.Trim(CHAR);
+            path3 = path3.Trim(CHAR);
+            path4 = path4.TrimStart(CHAR);
+            return $"{path1}.{path2}.{path3}.{path4}";
+        }
+
+        public static string Combine(params string[] paths)
+        {
+            if (paths is null)
+                throw new ArgumentNullException(nameof(paths));
+            if (paths.Length == 0)
+                return string.Empty;
+            if (paths.Length == 1)
+                return paths[0];
+
+            foreach (var path in paths)
+            {
+                if (string.IsNullOrEmpty(path))
+                    throw new ArgumentException($"“{nameof(paths)}”的一个或多个子项为 null 或空", nameof(paths));
+            }
+
+            StringBuilder sb = new();
+            sb.Append(paths[0].TrimEnd(CHAR));
+            for (int i = 1;  i < paths.Length - 1; i++)
+            {
+                sb.Append(CHAR);
+                sb.Append(paths[i].Trim(CHAR));
+            }
+            sb.Append(CHAR);
+            sb.Append(paths[^1].TrimStart(CHAR));
+
+            return sb.ToString();
+        }
+    }
+}
