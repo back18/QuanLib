e672789a1dbff9fea9572402b35073466e651e86
Quan <2200191123@qq.com>
2023-10-16 03:33:29 +0800

    完善 NullValidator

--- QuanLib.Core/NullValidator.cs
+++ QuanLib.Core/NullValidator.cs
@@ -41,24 +41,59 @@ namespace QuanLib.Core
             return nullFields.Length == 0 && nullProperties.Length == 0;
         }
 
-        public static void TryThrowException(object? instance, string name)
+        public static void ValidateObject(object instance, string name)
         {
+            if (instance is null)
+                throw new ArgumentNullException(nameof(instance));
             if (string.IsNullOrEmpty(name))
                 throw new ArgumentException($"“{nameof(name)}”不能为 null 或空。", nameof(name));
-            if (instance is null)
-                throw new ArgumentNullException(name);
 
             if (TryValidateObject(instance, out var nullFields, out var nullProperties))
                 return;
 
-            StringBuilder sb = new();
-            sb.AppendLine("对象的一个或多个成员为null：");
+            StringBuilder message = new();
+            message.AppendLine("对象的一个或多个成员为null：");
             foreach (var field in nullFields)
-                sb.AppendLine($"字段“{field.Name}”为null");
+                message.AppendLine($"字段“{field.Name}”为null");
             foreach (var property in nullProperties)
-                sb.AppendLine($"属性“{property.Name}”为null");
+                message.AppendLine($"属性“{property.Name}”为null");
 
-            throw new ArgumentException(sb.ToString().TrimEnd(), name);
+            throw new ArgumentException(message.ToString().TrimEnd(), name);
+        }
+
+        public static bool TryValidateObject<T>(IEnumerable<T> enumerable, out int[] nullValueIndexs)
+        {
+            if (enumerable is null)
+                throw new ArgumentNullException(nameof(enumerable));
+
+            List<int> nullValueIndexList = new();
+            int index = 0;
+            foreach(T? value in enumerable)
+            {
+                if (value is null)
+                    nullValueIndexList.Add(index);
+                index++;
+            }
+
+            nullValueIndexs = nullValueIndexList.ToArray();
+            return nullValueIndexs.Length == 0;
+        }
+
+        public static void ValidateObject<T>(IEnumerable<T> enumerable, string name)
+        {
+            if (enumerable is null)
+                throw new ArgumentNullException(nameof(enumerable));
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentException($"“{nameof(name)}”不能为 null 或空。", nameof(name));
+
+            if (TryValidateObject(enumerable, out var nullValueIndexs))
+                return;
+
+            List<string> indexs = new();
+            foreach (int index in nullValueIndexs)
+                indexs.Add($"[{index}]");
+
+            throw new ArgumentException("集合的一个或多个子项为null：" + string.Join(", ", indexs), name);
         }
     }
 }
