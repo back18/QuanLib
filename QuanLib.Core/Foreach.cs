f7d5a64c3b272639594cd5716dbf2ba09e2e318a
Quan <2200191123@qq.com>
2023-10-30 00:46:57 +0800

    为 Foreach.Start 添加重载，允许在遍历时返回false中断

--- QuanLib.Core/Foreach.cs
+++ QuanLib.Core/Foreach.cs
@@ -31,6 +31,30 @@ namespace QuanLib.Core
             }
         }
 
+        public static void Start<TValue1, Tvalue2>(
+            IEnumerable<TValue1> enumerable1,
+            IEnumerable<Tvalue2> enumerable2,
+            Func<TValue1, Tvalue2, bool> body)
+        {
+            if (enumerable1 is null)
+                throw new ArgumentNullException(nameof(enumerable1));
+            if (enumerable2 is null)
+                throw new ArgumentNullException(nameof(enumerable2));
+            if (body is null)
+                throw new ArgumentNullException(nameof(body));
+
+            using var enumerator1 = enumerable1.GetEnumerator();
+            using var enumerator2 = enumerable2.GetEnumerator();
+            enumerator1.Reset();
+            enumerator2.Reset();
+
+            while (enumerator1.MoveNext() && enumerator2.MoveNext())
+            {
+                if (!body.Invoke(enumerator1.Current, enumerator2.Current))
+                    break;
+            }
+        }
+
         public static void Start<TValue1, TValue2, TValue3>(
             IEnumerable<TValue1> enumerable1,
             IEnumerable<TValue2> enumerable2,
@@ -56,6 +80,35 @@ namespace QuanLib.Core
             while (enumerator1.MoveNext() && enumerator2.MoveNext() && enumerator3.MoveNext())
             {
                 body.Invoke(enumerator1.Current, enumerator2.Current, enumerator3.Current);
+            }
+        }
+
+        public static void Start<TValue1, TValue2, TValue3>(
+            IEnumerable<TValue1> enumerable1,
+            IEnumerable<TValue2> enumerable2,
+            IEnumerable<TValue3> enumerable3,
+            Func<TValue1, TValue2, TValue3, bool> body)
+        {
+            if (enumerable1 is null)
+                throw new ArgumentNullException(nameof(enumerable1));
+            if (enumerable2 is null)
+                throw new ArgumentNullException(nameof(enumerable2));
+            if (enumerable3 is null)
+                throw new ArgumentNullException(nameof(enumerable3));
+            if (body is null)
+                throw new ArgumentNullException(nameof(body));
+
+            using var enumerator1 = enumerable1.GetEnumerator();
+            using var enumerator2 = enumerable2.GetEnumerator();
+            using var enumerator3 = enumerable3.GetEnumerator();
+            enumerator1.Reset();
+            enumerator2.Reset();
+            enumerator3.Reset();
+
+            while (enumerator1.MoveNext() && enumerator2.MoveNext() && enumerator3.MoveNext())
+            {
+                if (!body.Invoke(enumerator1.Current, enumerator2.Current, enumerator3.Current))
+                    break;
             }
         }
 
@@ -89,6 +142,40 @@ namespace QuanLib.Core
             while (enumerator1.MoveNext() && enumerator2.MoveNext() && enumerator3.MoveNext() && enumerator4.MoveNext())
             {
                 body.Invoke(enumerator1.Current, enumerator2.Current, enumerator3.Current, enumerator4.Current);
+            }
+        }
+
+        public static void Start<TValue1, TValue2, TValue3, TValue4>(
+            IEnumerable<TValue1> enumerable1,
+            IEnumerable<TValue2> enumerable2,
+            IEnumerable<TValue3> enumerable3,
+            IEnumerable<TValue4> enumerable4,
+            Func<TValue1, TValue2, TValue3, TValue4, bool> body)
+        {
+            if (enumerable1 is null)
+                throw new ArgumentNullException(nameof(enumerable1));
+            if (enumerable2 is null)
+                throw new ArgumentNullException(nameof(enumerable2));
+            if (enumerable3 is null)
+                throw new ArgumentNullException(nameof(enumerable3));
+            if (enumerable4 is null)
+                throw new ArgumentNullException(nameof(enumerable4));
+            if (body is null)
+                throw new ArgumentNullException(nameof(body));
+
+            using var enumerator1 = enumerable1.GetEnumerator();
+            using var enumerator2 = enumerable2.GetEnumerator();
+            using var enumerator3 = enumerable3.GetEnumerator();
+            using var enumerator4 = enumerable4.GetEnumerator();
+            enumerator1.Reset();
+            enumerator2.Reset();
+            enumerator3.Reset();
+            enumerator4.Reset();
+
+            while (enumerator1.MoveNext() && enumerator2.MoveNext() && enumerator3.MoveNext() && enumerator4.MoveNext())
+            {
+                if (!body.Invoke(enumerator1.Current, enumerator2.Current, enumerator3.Current, enumerator4.Current))
+                    break;
             }
         }
     }
