56dcfcd8ece1f76535302644e5608734aabed2c9
Quan <2200191123@qq.com>
2023-10-31 02:07:38 +0800

    添加UnmanagedBase，提供通用的非托管资源释放方式

---
+++ QuanLib.Core/UnmanagedBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    public abstract class UnmanagedBase : IDisposable
+    {
+        protected UnmanagedBase()
+        {
+            IsDisposed = false;
+        }
+
+        protected readonly object _lock = new();
+
+        public bool IsDisposed { get; protected set; }
+
+        protected abstract void DisposeUnmanaged();
+
+        protected void Dispose(bool disposing)
+        {
+            lock (_lock)
+            {
+                if (IsDisposed || !disposing)
+                    return;
+
+                DisposeUnmanaged();
+                IsDisposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~UnmanagedBase()
+        {
+            Dispose(disposing: false);
+        }
+    }
+}
