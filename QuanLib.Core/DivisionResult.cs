3dee254127b0521fa5113e014d41b6e85d9e8b9f
Quan <2200191123@qq.com>
2023-11-24 14:24:38 +0800

    添加 Proportion 和 DivisionResult

---
+++ QuanLib.Core/DivisionResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    public readonly struct DivisionResult(int result, int remainder)
+    {
+        public readonly int Result = result;
+
+        public readonly int Remainder = remainder;
+
+        public static DivisionResult Compute(int left, int right)
+        {
+            int result = left / right;
+            int remainder = right % left;
+            return new(result, remainder);
+        }
+    }
+}
---
+++ QuanLib.Core/Proportion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    public readonly struct Proportion(int child, int parent)
+    {
+        public int Child { get; } = child;
+
+        public int Parent { get; } = parent;
+
+        public DivisionResult GetDivisionResult()
+        {
+            return DivisionResult.Compute(Child, Parent);
+        }
+
+        public double GetRatio()
+        {
+            return (double)Child / Parent;
+        }
+
+        public double GetRatio(int digits)
+        {
+            return Math.Round((double)Child / Parent, digits);
+        }
+
+        public double GetRatio(int digits, MidpointRounding mode)
+        {
+            return Math.Round((double)Child / Parent, digits, mode);
+        }
+
+        public double GetPercentage()
+        {
+            return (double)Child / Parent * 100;
+        }
+
+        public double GetPercentage(int digits)
+        {
+            return Math.Round((double)Child / Parent * 100, digits);
+        }
+
+        public double GetPercentage(int digits, MidpointRounding mode)
+        {
+            return Math.Round((double)Child / Parent * 100, digits, mode);
+        }
+    }
+}
