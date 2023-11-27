5762a9fda988d9848514447ad60d93bff6ddd1a7
Quan <2200191123@qq.com>
2023-11-28 01:52:01 +0800

    添加 public bool[,] GetBitArray(int pixelSize) 重载，用于获取不同像素大小的字模

--- QuanLib.BDF/FontData.cs
+++ QuanLib.BDF/FontData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLib.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -30,12 +31,37 @@ namespace QuanLib.BDF
 
         public int YOffset { get; }
 
-        public bool[,] GetBinary()
+        public bool[,] GetBitArray()
         {
             bool[,] result = new bool[Width, Height];
             for (int y = 0; y < Height; y++)
                 for (int x = 0; x < Width; x++)
                     result[x, y] = _data[y][x];
+            return result;
+        }
+
+        public bool[,] GetBitArray(int pixelSize)
+        {
+            ThrowHelper.ArgumentOutOfMin(1, pixelSize, nameof(pixelSize));
+
+            if (pixelSize == 1)
+                return GetBitArray();
+
+            Size size = new(Width * pixelSize, Height * pixelSize);
+            bool[,] result = new bool[size.Width, size.Height];
+            for (int y1 = 0, y2 = 0; y1 < Height; y1++, y2 += pixelSize)
+                for (int x1 = 0, x2 = 0; x1 < Width; x1++, x2 += pixelSize)
+                {
+                    if (_data[y1][x1])
+                    {
+                        int yend = y2 + pixelSize;
+                        int xend = x2 + pixelSize;
+                        for (int y3 = y2; y3 < yend; y3++)
+                            for (int x3 = x2; x3 < xend; x3++)
+                                result[x3, y3] = true;
+                    }
+                }
+
             return result;
         }
     }
