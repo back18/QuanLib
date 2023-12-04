1277a38db4af3aa51e0b53dc7dda9f1e4c8a801c
Quan <2200191123@qq.com>
2023-12-04 16:58:41 +0800

    添加工具类

---
+++ QuanLib.Downloader/DownloaderUtil.cs
@@ -0,0 +1,32 @@
+﻿using QuanLib.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Downloader
+{
+    public static class DownloaderUtil
+    {
+        public static string FormatProgressBar(long totalBytes, long downloadedBytes, int length, char background = '□', char foreground = '■')
+        {
+            ThrowHelper.ArgumentOutOfMin(0, totalBytes, nameof(totalBytes));
+            ThrowHelper.ArgumentOutOfRange(0, totalBytes, downloadedBytes, nameof(downloadedBytes));
+            ThrowHelper.ArgumentOutOfMin(0, length, nameof(length));
+
+            if (length == 0)
+                return string.Empty;
+
+            double progress = (double)downloadedBytes / totalBytes;
+            int foregroundLength = (int)Math.Round(progress * length);
+            int backgroundLength = length - foregroundLength;
+
+            StringBuilder sb = new();
+            sb.Append(foreground, foregroundLength);
+            sb.Append(background, backgroundLength);
+
+            return sb.ToString();
+        }
+    }
+}
