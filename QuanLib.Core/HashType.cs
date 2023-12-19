186c32ad3cdd76fe50abc21a57ad2a4c8eb46cea
Quan <2200191123@qq.com>
2023-12-20 01:03:46 +0800

    同步命名空间

--- QuanLib.Core/HashType.cs
+++ QuanLib.Core/HashType.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QuanLib.Core.IO
+namespace QuanLib.Core
 {
     /// <summary>
     /// 哈希算法类型
--- QuanLib.Core/PathType.cs
+++ QuanLib.Core/PathType.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QuanLib.Core.IO
+namespace QuanLib.Core
 {
     public enum PathType
     {
