e121f28cd4d0564eff1bdf45d3880179c019b90b
Quan <2200191123@qq.com>
2023-12-25 01:12:31 +0800

    添加 CheckHelper

---
+++ QuanLib.Core/CheckHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    public static class CheckHelper
+    {
+        public static bool Range<T>(T min, T max, T value) where T : IComparable
+        {
+            return value.CompareTo(min) >= 0 && value.CompareTo(max) <= 0;
+        }
+
+        public static bool Min<T>(T value, T min) where T : IComparable
+        {
+            return value.CompareTo(min) >= 0;
+        }
+
+        public static bool Max<T>(T value, T max) where T : IComparable
+        {
+            return value.CompareTo(max) <= 0;
+        }
+    }
+}
