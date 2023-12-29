6cb6950b39cb0343b4fb6753cf68f853f279f72a
Quan <2200191123@qq.com>
2023-12-29 16:28:30 +0800

    添加 IDataModel<TSelf, TModel> 接口

---
+++ QuanLib.Core/IDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    public interface IDataModel<TSelf, TModel>
+    {
+        public TModel ToDataModel();
+
+        public static abstract TSelf FromDataModel(TModel model);
+
+        public static abstract void ValidateDataModel(TModel model, string name);
+    }
+}
