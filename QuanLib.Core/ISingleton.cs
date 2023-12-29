172daa9f9581091adff13532e23cd0ac5f4c5d2a
Quan <2200191123@qq.com>
2023-12-29 15:51:02 +0800

    添加 ISingleton<TSelf, TArgs> 接口

---
+++ QuanLib.Core/ISingleton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    public interface ISingleton<TSelf, TArgs> where TArgs : InstantiateArgs
+    {
+        public static abstract bool IsInstanceLoaded { get; }
+
+        public static abstract TSelf Instance { get; }
+
+        public static abstract TSelf LoadInstance(TArgs args);
+    }
+}
---
+++ QuanLib.Core/InstantiateArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    public class InstantiateArgs
+    {
+        public readonly InstantiateArgs Empty = new();
+    }
+}
