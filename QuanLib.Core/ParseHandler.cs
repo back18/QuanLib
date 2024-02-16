87f8bec11d3929aaec609cf6911142c538749708
Quan <2200191123@qq.com>
2024-02-16 22:45:42 +0800

    添加委托 ParseHandler 和 TryParseHandler

---
+++ QuanLib.Core/ParseHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    public delegate T ParseHandler<T>(string s, IFormatProvider? provider);
+}
---
+++ QuanLib.Core/TryParseHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    public delegate bool TryParseHandler<T>([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out T result);
+}
