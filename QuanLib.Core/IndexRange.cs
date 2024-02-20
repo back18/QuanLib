fd3acaba3e18ef1dbade3cdb3cf1049fa4d45ed6
Quan <2200191123@qq.com>
2024-02-20 23:18:42 +0800

    添加类 ValueRange 和 IndexRange

---
+++ QuanLib.Core/IndexRange.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    public readonly struct IndexRange
+    {
+        public IndexRange(int start, int end)
+        {
+            ThrowHelper.ArgumentOutOfMin(0, start, nameof(start));
+            ThrowHelper.ArgumentOutOfMin(start, end, nameof(end));
+
+            Start = start;
+            End = end;
+        }
+
+        public int Start { get; }
+
+        public int End { get; }
+
+        public bool IsWithinRange(int index)
+        {
+            return index >= Start && index <= End;
+        }
+    }
+}
---
+++ QuanLib.Core/ValueRange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    public readonly struct ValueRange<T> where T : IComparable
+    {
+        public ValueRange(T min, T max)
+        {
+            ArgumentNullException.ThrowIfNull(min, nameof(min));
+            ArgumentNullException.ThrowIfNull(max, nameof(max));
+            ThrowHelper.ArgumentOutOfMin(min, max, nameof(max));
+
+            Min = min;
+            Max = max;
+        }
+
+        public T Min { get; }
+
+        public T Max { get; }
+
+        public readonly bool IsWithinRange(T value)
+        {
+            ArgumentNullException.ThrowIfNull(value, nameof(value));
+
+            return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+        }
+    }
+}
