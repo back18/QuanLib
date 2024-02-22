2704160a8e77320abdc8b1f9bec18ad61dc829cb
Quan <2200191123@qq.com>
2024-02-23 00:32:29 +0800

    添加类 NameAttribute 和 DescriptionAttribute

---
+++ QuanLib.Core/Attributes/DescriptionAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = true, AllowMultiple = false)]
+    public sealed class DescriptionAttribute : Attribute
+    {
+        public DescriptionAttribute(string description)
+        {
+            ArgumentNullException.ThrowIfNull(description, nameof(description));
+
+            Description = description;
+        }
+
+        public string Description { get; }
+    }
+}
---
+++ QuanLib.Core/Attributes/NameAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = true, AllowMultiple = false)]
+    public sealed class NameAttribute : Attribute
+    {
+        public NameAttribute(string name)
+        {
+            ArgumentNullException.ThrowIfNull(name, nameof(name));
+
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
