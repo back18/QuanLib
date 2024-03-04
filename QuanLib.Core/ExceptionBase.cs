fcf901f6e9faef9bb6279988501690e89810f1c9
Quan <2200191123@qq.com>
2024-03-04 23:08:07 +0800

    添加 ExceptionBase 类

---
+++ QuanLib.Core/ExceptionBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    public abstract class ExceptionBase : Exception
+    {
+        public ExceptionBase() : base(null)
+        {
+            InitialMessage = DefaultMessage;
+        }
+
+        public ExceptionBase(string? messge) : base(null)
+        {
+            InitialMessage = messge;
+        }
+
+        public ExceptionBase(string? messge, Exception innerException) : base(null, innerException)
+        {
+            InitialMessage = messge;
+        }
+
+        public override string Message
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(InitialMessage))
+                    return InitialMessage;
+                else
+                    return DefaultMessage;
+            }
+        }
+
+        protected virtual string? InitialMessage { get; set; }
+
+        protected abstract string DefaultMessage { get; }
+    }
+}
