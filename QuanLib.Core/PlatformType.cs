de9d38b41aa4a56ba87d2304243f8e85b96f610c
Quan2 <2200191123@qq.com>
2024-03-25 15:20:39 +0800

    为 QuanLib.Core项目添加了一些工具类

---
+++ QuanLib.Core/PlatformType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    [Flags]
+    public enum PlatformType
+    {
+        None = 0,
+
+        Windows = 1,
+
+        Linux = 2,
+
+        MacOS = 4
+    }
+}
---
+++ QuanLib.Core/ValidationHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    public static class ValidationHelper
+    {
+        public static void Validate(object instance, string name)
+        {
+            ArgumentNullException.ThrowIfNull(instance, nameof(instance));
+            ArgumentException.ThrowIfNullOrEmpty(name, nameof(name));
+
+            List<ValidationResult> results = [];
+            if (!Validator.TryValidateObject(instance, new(instance), results, true))
+            {
+                StringBuilder message = new();
+                message.AppendLine();
+                int count = 0;
+
+                foreach (var result in results)
+                {
+                    message.AppendLine(result.ErrorMessage);
+                    count++;
+                }
+
+                if (count > 0)
+                {
+                    message.Insert(0, $"解析“{name}”时遇到{count}个错误：");
+                    throw new ValidationException(message.ToString().TrimEnd());
+                }
+            }
+        }
+    }
+}
