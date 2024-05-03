bc01a0024165715afa36d744cfee8aa546160666
Quan2 <2200191123@qq.com>
2024-05-03 23:43:41 +0800

    完善 ObjectFormatter 类

--- QuanLib.DataAnnotations/ObjectFormatter.cs
+++ QuanLib.DataAnnotations/ObjectFormatter.cs
@@ -9,23 +9,78 @@ namespace QuanLib.DataAnnotations
 {
     public static class ObjectFormatter
     {
-        public static string Format(object? value)
+        public static string Format(object? obj)
         {
-            if (value is null)
-                return "null";
+            if (obj is null)
+                return "<null>";
 
-            if (value is string str)
-                return $"\"{str}\"";
+            if (obj is string text)
+                return Format(text);
 
-            if (value is IEnumerable enumerable)
+            if  (obj is Type type)
+                return Format(type);
+
+            if (obj is Exception exception)
+                return Format(exception.Message);
+
+            if (obj is IEnumerable enumerable)
+                return Format(enumerable);
+
+            return obj?.ToString() ?? Format((object?)null);
+        }
+
+        public static string Format(string text)
+        {
+            ArgumentNullException.ThrowIfNull(text, nameof(text));
+
+            return $"\"{text}\"";
+        }
+
+        public static string Format(Type type)
+        {
+            ArgumentNullException.ThrowIfNull(type, nameof(type));
+
+            StringBuilder stringBuilder = new();
+
+            stringBuilder.Append(type.Namespace);
+            stringBuilder.Append('.');
+            stringBuilder.Append(type.Name.Replace("`" + type.GenericTypeArguments.Length, string.Empty));
+
+            if (type.GenericTypeArguments.Length > 0)
             {
-                List<string> list = [];
-                foreach (var item in enumerable)
-                    list.Add(Format(item));
-                return $"[{string.Join(", ", list)}]";
+                stringBuilder.Append('<');
+                stringBuilder.AppendJoin(", ", type.GenericTypeArguments.Select(Format));
+                stringBuilder.Append('>');
             }
 
-            return value.ToString() ?? "null";
+            return stringBuilder.ToString();
+        }
+
+        public static string Format(Exception exception)
+        {
+            ArgumentNullException.ThrowIfNull(exception, nameof(exception));
+
+            string s = $"{Format(exception.GetType())}: {Format(exception.Message)}";
+            if (exception.InnerException is not null)
+                s += $" (InnerException: {Format(exception.InnerException)})";
+            return s;
+        }
+
+        public static string Format<T>(IEnumerable<T> enumerable)
+        {
+            ArgumentNullException.ThrowIfNull(enumerable, nameof(enumerable));
+
+            return $"[{string.Join(", ", enumerable.Select(s => Format(s)))}]";
+        }
+
+        public static string Format(IEnumerable enumerable)
+        {
+            ArgumentNullException.ThrowIfNull(enumerable, nameof(enumerable));
+
+            List<string> list = [];
+            foreach (var item in enumerable)
+                list.Add(Format(item));
+            return $"[{string.Join(", ", list)}]";
         }
     }
 }
