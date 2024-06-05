634549aec988344a3d5417779c312984995c9a8a
Quan <2200191123@qq.com>
2024-06-05 12:21:47 +0800

    添加 ProgressBar 类

---
+++ QuanLib.Core/ProgressBar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    public struct ProgressBar
+    {
+        public ProgressBar(int total)
+        {
+            ThrowHelper.ArgumentOutOfMin(0, total, nameof(total));
+
+            Total = total;
+            Current = 0;
+            Length = 10;
+            ProgressCharacter = '■';
+            BackgroundCharacter = '□';
+        }
+
+        public int Total { get; }
+
+        public int Current { get; set; }
+
+        public int Length { get; set; }
+
+        public char ProgressCharacter { get; set; }
+
+        public char BackgroundCharacter { get; set; }
+
+        public override readonly string ToString()
+        {
+            if (Length <= 0)
+                return string.Empty;
+
+            if (Total == 0 || Current > Total)
+                return new string(ProgressCharacter, Length);
+
+            double progress = (double)Current / Total;
+            int leftCount = (int)Math.Round(Length * progress);
+            int rightCount = Length - leftCount;
+            StringBuilder stringBuilder = new();
+            stringBuilder.Append(ProgressCharacter, leftCount);
+            stringBuilder.Append(BackgroundCharacter, rightCount);
+
+            return stringBuilder.ToString();
+        }
+    }
+}
