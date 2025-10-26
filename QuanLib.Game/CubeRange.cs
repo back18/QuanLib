08e434e8cffd34c1f6022cf527d944117824eaa0
back18 <2200191123@qq.com>
2025-10-26 23:19:29 +0800

    为 CubeRange 结构体新增方法

--- QuanLib.Game/CubeRange.cs
+++ QuanLib.Game/CubeRange.cs
@@ -18,7 +18,7 @@ namespace QuanLib.Game
         public readonly Vector3<int> Range => new(
             Math.Abs(EndPosition.X - StartPosition.X) + 1,
             Math.Abs(EndPosition.Y - StartPosition.Y) + 1,
-            Math.Abs(StartPosition.Z - EndPosition.Z) + 1);
+            Math.Abs(EndPosition.Z - StartPosition.Z) + 1);
 
         public readonly int Volume
         {
@@ -27,6 +27,76 @@ namespace QuanLib.Game
                 Vector3<int> range = Range;
                 return range.X * range.Y * range.Z;
             }
+        }
+
+        public readonly CubeRange Normalize()
+        {
+            Vector3<int> startPosition = new(Math.Min(StartPosition.X, EndPosition.X), Math.Min(StartPosition.Y, EndPosition.Y), Math.Min(StartPosition.Z, EndPosition.Z));
+            Vector3<int> endPosition = new(Math.Max(StartPosition.X, EndPosition.X), Math.Max(StartPosition.Y, EndPosition.Y), Math.Max(StartPosition.Z, EndPosition.Z));
+            return new CubeRange(startPosition, endPosition);
+        }
+
+        public readonly bool IsOverlaps(CubeRange other, bool normalize = true)
+        {
+            CubeRange source;
+            if (normalize)
+            {
+                source = Normalize();
+                other = other.Normalize();
+            }
+            else
+            {
+                source = this;
+            }
+
+            return !(other.StartPosition.X > source.EndPosition.X ||
+                    other.EndPosition.X < source.StartPosition.X ||
+                    other.StartPosition.Y > source.EndPosition.Y ||
+                    other.EndPosition.Y < source.StartPosition.Y ||
+                    other.StartPosition.Z > source.EndPosition.Z ||
+                    other.EndPosition.Z < source.StartPosition.Z);
+        }
+
+        public readonly bool IsContains(CubeRange other, bool normalize = true)
+        {
+            CubeRange source;
+            if (normalize)
+            {
+                source = Normalize();
+                other = other.Normalize();
+            }
+            else
+            {
+                source = this;
+            }
+
+            return other.StartPosition.X >= source.StartPosition.X &&
+                   other.EndPosition.X <= source.EndPosition.X &&
+                   other.StartPosition.Y >= source.StartPosition.Y &&
+                   other.EndPosition.Y <= source.EndPosition.Y &&
+                   other.StartPosition.Z >= source.StartPosition.Z &&
+                   other.EndPosition.Z <= source.EndPosition.Z;
+        }
+
+        public readonly bool IsEquals(CubeRange other, bool normalize = true)
+        {
+            CubeRange source;
+            if (normalize)
+            {
+                source = Normalize();
+                other = other.Normalize();
+            }
+            else
+            {
+                source = this;
+            }
+
+            return other.StartPosition.X == source.StartPosition.X &&
+                   other.EndPosition.X == source.EndPosition.X &&
+                   other.StartPosition.Y == source.StartPosition.Y &&
+                   other.EndPosition.Y == source.EndPosition.Y &&
+                   other.StartPosition.Z == source.StartPosition.Z &&
+                   other.EndPosition.Z == source.EndPosition.Z;
         }
     }
 }
