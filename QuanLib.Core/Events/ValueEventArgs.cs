a1a605de5ab99d3004809d9befdad86022f51a56
back18 <2200191123@qq.com>
2025-10-30 01:13:49 +0800

    添加值类型事件参数处理

---
+++ QuanLib.Core/Events/ValueEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core.Events
+{
+    public readonly struct ValueEventArgs : IValueEventArgs
+    {
+        public static readonly ValueEventArgs Empty = default;
+    }
+}
---
+++ QuanLib.Core/ValueEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using QuanLib.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Core
+{
+    public delegate void ValueEventHandler<TSender, TEventArgs>(TSender sender, TEventArgs e) where TEventArgs : IValueEventArgs;
+}
