24e64bdb818db906477ff160a8917b82e50aeafc
back18 <2200191123@qq.com>
2025-11-11 21:55:36 +0800

    添加检查TCP监听器与TCP连接的工具方法

--- QuanLib.Core/NetworkUtil.cs
+++ QuanLib.Core/NetworkUtil.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,29 @@ namespace QuanLib.Core
 {
     public static class NetworkUtil
     {
+        public static bool TcpListenerIsActive(int port)
+        {
+            ThrowHelper.ArgumentOutOfRange(IPEndPoint.MinPort, IPEndPoint.MaxPort, port, nameof(port));
+
+            IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] listeners = properties.GetActiveTcpListeners();
+            return listeners.Any(s => s.Port == port);
+        }
+
+        public static bool TcpConnectionIsActive(int port)
+        {
+            ThrowHelper.ArgumentOutOfRange(IPEndPoint.MinPort, IPEndPoint.MaxPort, port, nameof(port));
+
+            IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+            TcpConnectionInformation[] connections = properties.GetActiveTcpConnections();
+            return connections.Any(s => s.LocalEndPoint.Port == port);
+        }
+
         public static bool TestTcpConnectivity(IPAddress address, int port)
         {
+            ArgumentNullException.ThrowIfNull(address, nameof(address));
+            ThrowHelper.ArgumentOutOfRange(IPEndPoint.MinPort, IPEndPoint.MaxPort, port, nameof(port));
+
             TcpClient client = new();
             try
             {
@@ -30,6 +52,9 @@ namespace QuanLib.Core
 
         public static async Task<bool> TestTcpConnectivityAsync(IPAddress address, int port)
         {
+            ArgumentNullException.ThrowIfNull(address, nameof(address));
+            ThrowHelper.ArgumentOutOfRange(IPEndPoint.MinPort, IPEndPoint.MaxPort, port, nameof(port));
+
             TcpClient client = new();
             try
             {
