3c2b35ff6c115c1dcda5b0f6ecb9eb9ece726ba9
back18 <2200191123@qq.com>
2026-02-12 01:47:17 +0800

    修正 ValueRange<T> 结构体的泛型约束

--- QuanLib.Core/ValueRange.cs
+++ QuanLib.Core/ValueRange.cs
@@ -6,12 +6,10 @@ using System.Threading.Tasks;
 
 namespace QuanLib.Core
 {
-    public readonly struct ValueRange<T> where T : IComparable
+    public readonly struct ValueRange<T> where T : struct, IComparable<T>
     {
         public ValueRange(T min, T max)
         {
-            ArgumentNullException.ThrowIfNull(min, nameof(min));
-            ArgumentNullException.ThrowIfNull(max, nameof(max));
             ThrowHelper.ArgumentOutOfMin(min, max, nameof(max));
 
             Min = min;
@@ -24,8 +22,6 @@ namespace QuanLib.Core
 
         public readonly bool IsWithinRange(T value)
         {
-            ArgumentNullException.ThrowIfNull(value, nameof(value));
-
             return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
         }
     }
