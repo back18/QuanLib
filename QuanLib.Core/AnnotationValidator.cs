721308121fe88e5c48ede4e00e3e0c694dcce141
back18 <2200191123@qq.com>
2026-03-19 01:06:58 +0800

    新增验证器接口及注解验证器实现
    - 定义 IValidator 接口，包含 IsValid 和 Validate 方法
    - 实现 AnnotationValidator，支持多个 ValidationAttribute 的链式验证
    - 验证失败时抛出包含参数名的异常

---
+++ QuanLib.Core/AnnotationValidator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace QuanLib.Core
+{
+    public class AnnotationValidator : IValidator
+    {
+        public AnnotationValidator(ValidationAttribute[] validationAttributes)
+        {
+            ArgumentNullException.ThrowIfNull(validationAttributes, nameof(validationAttributes));
+
+            _validationAttributes = validationAttributes;
+        }
+
+        private readonly ValidationAttribute[] _validationAttributes;
+
+        public bool IsValid(object? value)
+        {
+            if (_validationAttributes.Length == 0)
+                return true;
+
+            foreach(ValidationAttribute validationAttribute in _validationAttributes)
+            {
+                if (!validationAttribute.IsValid(value))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public void Validate(object? value, string name)
+        {
+            if (_validationAttributes.Length == 0)
+                return;
+
+            foreach (ValidationAttribute validationAttribute in _validationAttributes)
+                validationAttribute.Validate(value, name);
+        }
+    }
+}
---
+++ QuanLib.Core/IValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuanLib.Core
+{
+    public interface IValidator
+    {
+        public bool IsValid(object? value);
+
+        public void Validate(object? value, string name);
+    }
+}
